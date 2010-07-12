1841d9fb5f124334c1d726f0329444cb4d643a77
Mauricio Murga <murga.mauricio@gmail.com>
2010-07-12 12:40:52 -0500

    Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :actions
   validates_presence_of :screen_name
+  validates_format_of :email, :with => /\A([^@\s]+)@[^(?!yahoo)(?!aol)(?!gmail)(?!msn)(?!comcast)(?!hotmail)(?!live)(?!bing)(?!altavista)]((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 
   acts_as_authentic do |c|
      c.login_field = 'email'
