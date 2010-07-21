50a0daf1d4b03b9df7ecc91b2dab52412344773f
Mauricio Murga <murga.mauricio@gmail.com>
2010-07-21 13:15:14 -0500

    activation mail test

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ActionMailer::Base
  def welcome_email(user)
   recipients user.email
-  from "El directorio de companias <no-reply@eldirectorio.com>"
+  from "El directorio de companias <no-reply@contacto.com>"
   subject "Bienvenido al directorio de companias"
   sent_on Time.now
- body :user => user, :url => "http://contacto.heroku.com/"
+ body :user => user
+ @url => "http://contacto.heroku.com/"
  end
 end 
