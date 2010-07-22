8671fe00acf1873ce4120738018d835c5f6bdeef
Mauricio Murga <murga.mauricio@gmail.com>
2010-07-22 15:38:54 -0500

    invite

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,17 +1,16 @@
 class UserMailer < ActionMailer::Base
-@url = "http://contacto.heroku.com/"
  def welcome_email(user)
   recipients user.email
   from "El directorio de companias <no-reply@contacto.com>"
   subject "¡Bienvenido al directorio de compañías más grande!"
   sent_on Time.now
- body :user => user
+ body :user => user, :url => "http://contacto.heroku.com/"
  end
  def invite_email(user)
   recipients user
   from "El directorio de companias <no-reply@contacto.com>"
   subject "¡Te han invitado al directorio de compañías más grande!"
   sent_on Time.now
- body :user => current_user
+ body :user => user
  end
 end 
