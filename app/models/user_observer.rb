af7a5ba35a9ac2a7214f96e56e630ac81ea04f75
Mauricio Murga <murga.mauricio@gmail.com>
2010-07-22 15:15:23 -0500

    invite

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -2,4 +2,7 @@ class UserObserver < ActiveRecord::Observer
  def after_create(user)
   UserMailer.deliver_welcome_email(user)
  end
+ def after_invite(user)
+  UserMailer.deliver_invite_email(user)
+ end
 end 
--- app/observers/user_observer.rb
+++
@@ -1,8 +0,0 @@
-class UserObserver < ActiveRecord::Observer
- def after_create(user)
-  UserMailer.welcome_email(user).deliver
- end
- def after_invite(user)
-  UserMailer.invite_email(user).deliver
- end
-end 
