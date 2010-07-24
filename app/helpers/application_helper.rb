2f029953a09cdda39407d4c12de586d58ac8a685
Cristina <cristinarandall@gmail.com>
2010-07-24 09:30:03 -0500

    css changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+	def controller_stylesheet_link_tag
+  		stylesheet = "#{params[:controller]}.css"
+    
+  		if File.exists?(File.join(Rails.public_path, 'stylesheets', stylesheet))
+    		   stylesheet_link_tag stylesheet
+  		end
+	end
+
 end
