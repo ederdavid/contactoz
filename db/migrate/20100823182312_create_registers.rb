dc59e12848b2d66186c31a12efc256a9bc42d565
Cristina <cristinarandall@gmail.com>
2010-09-10 18:28:02 -0500

    message

---
+++ db/migrate/20100823182312_create_registers.rb
@@ -0,0 +1,11 @@
+class CreateRegisters < ActiveRecord::Migration
+  def self.up
+    create_table :registers do |t|
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :registers
+  end
+end
