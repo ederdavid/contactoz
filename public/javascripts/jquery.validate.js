8e88b494d4411aa71a07b540c028a71227e4d807
Cristina <cristinarandall@gmail.com>
2010-09-12 13:42:57 -0500

    message

--- public/javascripts/jquery.validate.js
+++ public/javascripts/jquery.validate.js
@@ -266,7 +266,7 @@ $.extend($.validator, {
 		equalTo: "Please enter the same value again.",
 		accept: "Please enter a value with a valid extension.",
 		maxlength: $.validator.format("Please enter no more than {0} characters."),
-		minlength: $.validator.format("Please enter at least {0} characters."),
+		minlength: $.validator.format("Al menos {0} caracteres."),
 		rangelength: $.validator.format("Please enter a value between {0} and {1} characters long."),
 		range: $.validator.format("Please enter a value between {0} and {1}."),
 		max: $.validator.format("Please enter a value less than or equal to {0}."),
