f6f76e61beca8820c8d3a7c72fd441946c7aa304
Cristina <cristinarandall@gmail.com>
2010-09-19 13:38:48 -0500

    message

--- db/migrate/20100730041939_add_company_index.rb
+++
@@ -1,9 +0,0 @@
-class AddCompanyIndex < ActiveRecord::Migration
-  def self.up
-	 add_index :industries, [:company_id]
-  end
-
-  def self.down
-	remove_index :industries, [:company_id]
-  end
-end
