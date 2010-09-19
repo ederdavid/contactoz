eed5a60845312114a04277682d2418feead629ba
Cristina <cristinarandall@gmail.com>
2010-09-19 13:49:56 -0500

    message

--- db/migrate/20100802230942_add_company_id.rb
+++
@@ -1,9 +0,0 @@
-class AddCompanyId < ActiveRecord::Migration
-  def self.up
-	add_column :advertisements, :company_id, :int
-  end
-
-  def self.down
-	remove_column :advertisements, :company_id
-  end
-end
