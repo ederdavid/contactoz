d28255120fe36109b8eb186ee898a78ecb96b32d
Cristina <cristinarandall@gmail.com>
2010-09-26 15:15:17 -0500

    message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,12 +35,8 @@ class ApplicationController < ActionController::Base
   
   private  
 
-MOBILE_USER_AGENTS =  'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
-                          'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
-                          'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
-                          'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
-                          'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|' +
-                          'mobile'
+MOBILE_USER_AGENTS =  'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|mobile'
+
 def mobile_device?
   if session[:mobile_param]
     session[:mobile_param] == "1"
