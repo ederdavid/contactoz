0286c919a0b94987335bc94457cb322a29aa92f7
Cristina <cristinarandall@gmail.com>
2010-10-04 12:40:18 -0500

    landings

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -14,20 +14,5 @@ class LandingsController < ApplicationController
 
 
 
-  # POST /landings
-  # POST /landings.xml
-  def create
-    @landing = Landing.new(params[:landing])
-
-    respond_to do |format|
-      if @landing.save
-        format.html { redirect_to(@landing, :notice => 'Landing was successfully created.') }
-        format.xml  { render :xml => @landing, :status => :created, :location => @landing }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @landing.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
 
 end
