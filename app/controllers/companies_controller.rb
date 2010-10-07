260f240d40b72214d649f0116b49b72b803945c3
Mauricio Murga <murga.mauricio@gmail.com>
2010-10-07 13:31:59 -0500

    test

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -243,7 +243,7 @@ def sortByName
                end
 		#format.xml  { render :xml => Digest::MD5.hexdigest("#{@app_key}#{@parameters}#{@secret}").to_s 
            else
-               @test = Digest::MD5.hexdigest('#{@app_key}#{@parameters}#{@secret}').to_s
+               @test = Digest::MD5.hexdigest("#{@app_key}#{@parameters}#{@secret}").to_s
                format.xml  { render :xml => "<WARNING>El signature debe ser: #{@test} </WARNING>" }
            end
         end
@@ -266,7 +266,7 @@ def sortByName
                format.xml  { render :xml => @company.to_xml(:only => [:id, :company_name, :company_city, :company_state, :updated_at]) }
 		#format.xml  { render :xml => Digest::MD5.hexdigest("#{@app_key}#{@parameters}#{@secret}").to_s }
            else
-               @test = Digest::MD5.hexdigest('#{@app_key}#{@parameters}#{@secret}').to_s
+               @test = Digest::MD5.hexdigest("#{@app_key}#{@parameters}#{@secret}").to_s
                format.xml  { render :xml => "<WARNING>El signature debe ser: #{@test} </WARNING>" }
            end
         end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -40,7 +40,7 @@ class ServicesController < ApplicationController
                    format.xml  { render :xml => "<WARNING>there is not a service for that if</WARNING>" }
                end
            else
-               @test = Digest::MD5.hexdigest('#{@app_key}#{@parameters}#{@secret}').to_s
+               @test = Digest::MD5.hexdigest("#{@app_key}#{@parameters}#{@secret}").to_s
                format.xml  { render :xml => "<WARNING>El signature debe ser: #{@test} </WARNING>" }
            end
       end
@@ -78,7 +78,7 @@ class ServicesController < ApplicationController
                    format.xml  { render :xml => "<WARNING>there is not such a service</WARNING>" }
                end
            else
-               @test = Digest::MD5.hexdigest('#{@app_key}#{@parameters}#{@secret}').to_s
+               @test = Digest::MD5.hexdigest("#{@app_key}#{@parameters}#{@secret}").to_s
                format.xml  { render :xml => "<WARNING>El signature debe ser: #{@test} </WARNING>" }
            end
       end
