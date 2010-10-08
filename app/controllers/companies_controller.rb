ca26e37d0e74c464479334e39ab52ce9908b9fc8
Cristina <cristinarandall@gmail.com>
2010-10-08 17:21:45 -0500

    message

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,20 +4,6 @@ class CompaniesController < ApplicationController
     layout 'application'
     $global_page = 20
 
-def self.root
-    @companies= Company.all
-    for i in @companies
-        @i = i
-        @companyLocation = CompanyLocation.find_by_company_id(i.id)
-        if @companyLocation != nil
-            @i.root = true
-            @i.save
-        else
-            @i.root = false
-            @i.save
-        end
-    end
-end
 
 def clean
     @fd = File.open("/home/mauricio/contacto/MexicoJSON")
@@ -187,16 +173,12 @@ def sortByName
        @query = params[:search]
        if @query 
         	if params[:sort].nil?
-<<<<<<< HEAD
 			 #@companies = Company.paginate(:page=>params[:page],:per_page=> 20,:conditions, :conditions => ['company_name LIKE ?', "%#{params[:search]}%"],  :order => 'company_name')
 			
 			#@companies = Company.paginate(:page=>params[:page],:per_page=> 20,:conditions => ['(company_name like ? AND root = ?) OR (company_name like ? AND company_location_id is NULL)', "%#{params[:search]}%", "t", "%#{params[:search]}%"], :order => 'company_name')
 
 			@companies = Company.paginate(:page=>params[:page],:per_page=> 20,:conditions => ['company_name like ?', "%#{params[:search]}%"], :order => 'company_name')
 
-=======
-			@companies = Company.paginate(:page=>params[:page],:per_page=> 20,:conditions => ['company_name like ? AND (root = ? OR company_location_id is NULL)', "%#{params[:search]}%", "t"], :order => 'company_name')
->>>>>>> 2dccbdf8454e2f05ae9124a85b05f39f7bbf4f05
 		end       
        end
        respond_to do |format|
