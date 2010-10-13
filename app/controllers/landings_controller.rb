48278609bf5a2357c69e56f03f04adea9d60947e
Cristina <cristinarandall@gmail.com>
2010-10-12 23:54:30 -0500

    message

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -18,7 +18,7 @@ class LandingsController < ApplicationController
    
    @prospect = Prospect.create(:email => params[:email])
    if @prospect.save
-      flash[:notice] = "... recibirá un mensaje antes de su lanzamiento"
+      flash[:notice] = "... recibirás un mensaje antes de lanzamiento"
    end
 
        render :action => 'index'
